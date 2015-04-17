9a13f1e391d8a5ac0e12741f20f075dc1f7b54c0
root <root@ip-172-31-44-12.us-west-2.compute.internal>
2015-04-17 20:49:28 +0000

    updated nginx commands

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,16 @@ cd /
 mkdir /data
 cd data
 
+# GIT
+
+#apt-get install git
+git init
+git remote add origin http://github.com/markweinberg/MF.git
+git pull origin master
 
 # Setup nginx
 
+apt-get install nginx
+nginx -s stop
+nginx -c /data/nginx/nginx.conf
 
