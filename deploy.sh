865a0864463cf0b642b75e773311c6f88a5e5107
root <root@ip-172-31-44-12.us-west-2.compute.internal>
2015-04-18 01:32:23 +0000

    Added more sophisticated logic to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+function IsPackageInstalled()
+{
+   PKG_INSTALLED=$(dpkg-query -W --showformat='${Status}\n' $1 | grep "install ok installed")
+
+   if [ "" == "$PKG_INSTALLED" ]
+   then
+      return 0 
+   else
+      return 1
+   fi
+}
+
 export GIT_EDITOR=vi
 cd /
 mkdir /data
@@ -5,14 +19,32 @@ cd data
 
 # GIT
 
-#apt-get install git
-git init
+GIT_INSTALLED=$(IsPackageInstalled "git")
+
+if [ "" == "$GIT_INSTALLED" ]
+then
+   echo Installing git...
+   apt-get install git 
+   git init
+else
+   echo "git installed"
+fi
+
 git remote add origin http://github.com/markweinberg/MF.git
 git pull origin master
 
-# Setup nginx
+# NGINX
 
-apt-get install nginx
-nginx -s stop
-nginx -c /data/nginx/nginx.conf
+echo Verifying nginx install...
 
+NGINX_INSTALLED=$(IsPackageInstalled "nginx")
+
+if [ "" == "$NGINX_INSTALLED" ]
+then
+   echo Installing nginx...
+   apt-get install nginx
+   nginx -s stop
+   nginx -c /data/nginx/nginx.conf
+else
+   echo "nginx installed"
+fi
